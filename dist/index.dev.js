b7fcf89f517a2a327096ab913aeb9adfa21c4246
md nahid <mdnahid360s@gmail.com>
2023-03-27 23:07:18 +0600

    news and others api completed

--- dist/index.dev.js
+++ dist/index.dev.js
@@ -18,6 +18,28 @@ app.get('/', function (req, res) {
 app.get('/new-category', function (req, res) {
   res.send(category);
 });
+app.get('/news/:id', function (req, res) {
+  var nsId = req.params;
+  var findNews = news.find(function (nws) {
+    return nws._id === nsId.id;
+  });
+  res.send(findNews);
+});
+app.get('/category/:id', function (req, res) {
+  var ctId = req.params.id;
+
+  if (ctId === "08") {
+    res.send(news);
+  } else {
+    var cNews = news.filter(function (ns) {
+      return ns.category_id === ctId;
+    });
+    res.send(cNews);
+  }
+});
+app.get('/news', function (req, res) {
+  res.send(news);
+});
 app.listen(port, function () {
   console.log("Example app listening on port ".concat(port));
 });
--- index.js
+++ index.js
@@ -13,8 +13,27 @@ app.get('/', (req, res) => {
 
 app.get('/new-category', (req, res)=>{
     res.send(category)
+});
+
+app.get('/news/:id', (req, res)=>{
+  const nsId = req.params;
+  const findNews = news.find(nws => nws._id === nsId.id);
+  res.send(findNews )
 })
 
+app.get('/category/:id', (req, res)=>{
+  const ctId = req.params.id;
+    if (ctId === "08") {
+      res.send(news)
+    }else{
+      const cNews = news.filter(ns => ns.category_id === ctId);
+      res.send(cNews);
+    }
+})
+
+app.get('/news', (req, res)=>{
+  res.send(news)
+})
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
